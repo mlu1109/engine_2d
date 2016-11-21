8988ced10d48e7bcda69c8dec0280bffbbfeaf03
Matti Lundgren <matlu703@student.liu.se>
2016-11-21 06:02:19 +0100

    Generate random shapes

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
+#include <stdlib.h>
 #include "Render.hpp"
 #include "World.hpp"
 #include "Input.hpp"
 
 using namespace eng;
+
+
+Poly getRandomPoly(double width, double height)
+{
+	switch(rand() % 4)
+	{
+		case 0:
+			return Poly::createPerpendicular(width, height);
+		case 1:
+			return Poly::createEquilateral(width, height);
+		case 2:
+			return Poly::createRectangle(width, height);
+		case 3:
+		default:
+			return Poly::createPentagon(width, height);
+	}
+}
 
 int main()
 {
@@ -10,44 +28,29 @@ int main()
 	Render render;
 	Input input;
 
-	world.addObject(
-			PhysicsObject(
-					Vec(-100, -100),
-					Poly::createPentagon(50, 50),
-					2
-			)
-	);
-	world.addObject(
-			PhysicsObject(
-					Vec(100, 100),
-					Poly::createRectangle(100, 100),
-					10
-			)
-	);
-	world.addObject(
-			PhysicsObject(
-					Vec(200, 200),
-					Poly::createPerpendicular(100, 100),
-					10
-			)
-	);
-	world.addObject(
-			PhysicsObject(
-					Vec(0, 0),
-					Poly::createRectangle(1000, 5),
-					INFINITY
-			)
-	);
+	srand((unsigned) std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()));
 
-	world.addObject(
-			PhysicsObject(
-					Vec(0, 400),
-					Poly::createRectangle(1000, 5),
-					INFINITY
-			)
-	);
+	for (int i = 0; i < 500; ++i)
+	{
+		int s = rand() % 4;
+		int x = rand() % 2000;
+		int y = rand() % 2000;
+		int width = rand() % 200;
+		int height = rand() % 200;
+		int mass = rand() % 200;
+
+		world.addObject(
+				PhysicsObject(
+						Vec(x - x/2, y - y/2 ),
+						getRandomPoly(width, height),
+						mass
+				)
+		);
+	}
+
 
 	auto *controlled = world.getObject(0);
+	controlled->addAngVel(500);
 
 	while (input.running())
 	{
@@ -60,10 +63,10 @@ int main()
 			render.paintObjects(world.objects());
 			render.setColor(0xFF0000);
 			render.paintObject(*controlled);
-			for (const auto &c : world.collisions())
+			/*for (const auto &c : world.collisions())
 			{
 				render.paintDebugCollision(c);
-			}
+			}*/
 
 			render.update();
 		}
